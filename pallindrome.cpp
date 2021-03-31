42ff5cbe72edb9fe7796e0cc5828dcdf2511a7e3
Vatsal <vatsalsinha001@gmail.com>
2021-03-31 19:06:48 +0530

    pallindrome

---
+++ pallindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        //method1:
+        //string X = to_string(x);
+        //string X1 = to_string(x);
+        //reverse(X1.begin(), X1.end());
+        //if(X == X1){
+        //    return true;
+        //}
+        //return false;
+        //..........................................
+        //method2
+        if(x < 0){
+            return false;
+        }
+        int count = 0;
+        int temp = x;
+        int temp1;
+        temp = x;
+        long long res = 0;
+        //count = count - 1;
+        while(temp != 0 && res < INT_MAX){
+            temp1 = temp%10;
+            temp = temp / 10;
+            res = res*10 + temp1;
+        }
+        if (res == x){
+            return true;
+        }
+        return false;
+    }
+};
