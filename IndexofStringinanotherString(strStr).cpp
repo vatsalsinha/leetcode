60c8a7fa9a2e472702fec0dd248a8283de968cb7
Vatsal <vatsalsinha001@gmail.com>
2021-04-11 14:16:00 +0530

    str Str

---
+++ IndexofStringinanotherString(strStr).cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle == ""){
+            return 0;
+        }
+        return haystack.find(needle);
+    }
+};
