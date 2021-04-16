ce811f9ff2ba6ccadf886e52ed9fa7d7c3bf5aee
Vatsal <vatsalsinha001@gmail.com>
2021-04-17 01:37:36 +0530

    wildcard matching with comments

--- wildcardMatching.cpp
+++ wildcardMatching.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        /*
+        if p[i-1] == '*' : check one row up, one column left and diagonally up because if the string is being satisfied uptill now, its going to be satisifed as * can take any value any number of times.   
+        if p[i-1] == s[j-1] : check diagonally up  because if the string is being satisfied uptill now, its going to be satisifed as the characters are being matched.
+        if p[i-1] == '?' : check diagonally up because if the string is being satisfied uptill now, its going to be satisifed as ? can take any one character. 
+        */
         vector<vector<bool>> A(p.length()+1,vector<bool>(s.length()+1,0));
         A[0][0] = 1;
         for(int i = 0; i < A.size(); i++){
