002230371f2487cd458575c084614e9b44668260
Vatsal <vatsalsinha001@gmail.com>
2021-04-17 15:34:16 +0530

    permutations updated

--- permutations.cpp
+++ permutations.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    void findCombinations(int index, int last, vector<int> nums, vector<vector<int>> &ans, vector<int> &ds){
+    void findCombinations(int index, int last, vector<int> nums, vector<vector<int>> &ans){
         if(index == nums.size()){
             ans.push_back(nums);
             return;
         }
         int i = 0;
         for(int i = index; i < last+1; i++){
+            swap(nums[i], nums[index]);     
+            findCombinations(index+1, last, nums, ans);
             swap(nums[i], nums[index]);
-            findCombinations(index+1, last, nums, ans, ds);
-            swap(nums[i], nums[index]);
+            
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
@@ -18,8 +19,7 @@ public:
             res.push_back({nums[0]});
         }
         vector<vector<int>> ans;
-        vector<int> ds;
-        findCombinations(0, nums.size()-1, nums, ans, ds);
+        findCombinations(0, nums.size()-1, nums, ans);
         return ans;
     }
 };
