08bb921d148c7a92f5ab430b581afa1bb22f9242
Vatsal <vatsalsinha001@gmail.com>
2021-04-17 20:34:52 +0530

    rotate image

---
+++ rotateImage.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //transpose
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = i; j < matrix[i].size(); j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        //reverse
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size()/2; j++){
+                swap(matrix[i][j], matrix[i][matrix.size()-1-j]);
+            }
+        }
+    }
+};
