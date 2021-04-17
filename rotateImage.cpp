473bfd4352d8b0fb0ade3aff7fd4e8243e9dcaa2
Vatsal <vatsalsinha001@gmail.com>
2021-04-17 21:03:24 +0530

    rotate Image updated

--- rotateImage.cpp
+++ rotateImage.cpp
@@ -8,10 +8,13 @@ public:
             }
         }
         //reverse
+        // for(int i = 0; i < matrix.size(); i++){
+        //     for(int j = 0; j < matrix[i].size()/2; j++){
+        //         swap(matrix[i][j], matrix[i][matrix.size()-1-j]);
+        //     }
+        // }
         for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size()/2; j++){
-                swap(matrix[i][j], matrix[i][matrix.size()-1-j]);
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
