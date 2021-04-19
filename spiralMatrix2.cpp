2e8232b570834cc082c9da9a606d52deb7f7177e
Vatsal <vatsalsinha001@gmail.com>
2021-04-20 01:53:56 +0530

    spiral matrix 2

---
+++ spiralMatrix2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int> (n));
+        int min_row = 0;
+        int min_col = 0;
+        int max_row = n-1;
+        int max_col = n-1;
+        int tot = n*n;
+        int count = 1;
+        while(count <= tot){
+            //min row ----->>
+            for(int i = min_row, j = min_col; j <= max_col && count <= tot; j++){
+                res[min_row][j] = count++;
+               
+            }
+            min_row++;
+            //max_col |
+            //        v 
+            for(int i = min_row, j = max_col; i <= max_row && count <= tot; i++){
+                res[i][max_col] = count++;
+                
+            }
+            max_col--; 
+            //max_col <-------
+            for(int i = max_row, j = max_col; j >= min_col && count <= tot; j--){
+                res[max_row][j] = count++;
+              
+            }
+            max_row--;   
+            //max_row ^
+            //        |
+            for(int i = max_row, j = min_col; i >= min_row && count <= tot; i--){
+                res[i][min_col] = count++;
+                
+            }
+            min_col++;
+        }
+        return res;
+    }
+};
