8c0bd39c9656edd5937dbd912da447f382eb99da
Vatsal <vatsalsinha001@gmail.com>
2021-04-30 13:54:19 +0530

    largest rectangle in histogram

--- largestRectangleinHistogram.cpp
+++ largestRectangleinHistogram.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-       int rb[heights.size()], lb[heights.size()];
+    void fillLb(vector<int> heights, vector<int> &lb){
         stack<int> st;
-        st.push(heights.size()-1);
-        for(int i = heights.size()-1; i >= 0; i--){
-            while(st.size() > 0 && heights[i] <= heights[st.top()]){
-                st.pop();
-            }
-            rb[i] = st.empty() ? heights.size() : st.top();
-            st.push(i);
-        }
-        while(!st.empty()) st.pop();
         for(int i = 0; i < heights.size(); i++){
-            while(st.size() > 0 && heights[i] <= heights[st.top()]){
+            while(!st.empty() && heights[i] <= heights[st.top()])
                 st.pop();
-            }
             lb[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
-        int ans = 0, tmp;
-        for(int i = 0; i < heights.size(); i++){
-            ans = max(ans, heights[i]*(rb[i]-lb[i]-1));
+    }
+    void fillRb(vector<int> heights, vector<int> &rb){
+        stack<int> st;
+        for(int i = heights.size()-1; i > -1; i--){
+            while(!st.empty() && heights[i] <= heights[st.top()])
+                st.pop();
+            rb[i] = st.empty() ? heights.size() : st.top();
+            st.push(i);
+        }
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        if(heights.size() == 0)
+            return 0;
+        if(heights.size() == 1){
+            return heights[0];
+        }
+        vector<int> rb(heights.size()), lb(heights.size());
+        fillLb(heights, lb);
+        fillRb(heights,rb);
+        int ans = 0;
+        for(int i =0; i < heights.size(); i++){
+            ans = max(ans, (rb[i]-lb[i]-1)*heights[i]);
         }
         return ans;
     }
