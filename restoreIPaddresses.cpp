4e718394ef9af70efd4eb366d21173fe1bab225e
Vatsal <vatsalsinha001@gmail.com>
2021-05-03 17:18:12 +0530

    restore ip addresses updated

--- restoreIPaddresses.cpp
+++ restoreIPaddresses.cpp
@@ -1,35 +1,75 @@
 class Solution {
 public:
-    
-    bool isValid(string s){
-        if(s[0]=='0' && s.size()>1) return false;
-        int num=stoi(s);
-        return num>=0 && num<=255;
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> rt;
+        
+        backtracking(rt, s, 0, 1);
+        return rt;
     }
     
-    void backtrack(vector<string> &ans, string sans,int count,string s){
-        if(count>4) return;
-        if(count==4 && s.size()==0){
-            sans.pop_back();
-            ans.push_back(sans);
+    // level indicates how many '.'s inserted
+    void backtracking(vector<string> &rt, string& s, int index, int level){
+        if(level == 4){
+            if(s.size()-index <= 3 && s.size()-index > 0 && check(s))
+                rt.push_back(s);
             return;
         }
-        else if(count==4 || s.size()==0) return;
         
-        for(int i=1;i < 4 && i <= s.size();i++){
-            string sub=s.substr(0,i);
-            if(isValid(sub)){
-                backtrack(ans, sans+sub+".",count+1,s.substr(i));
+        for(int i=index+1; i<=index+3 && i<s.size(); i++){
+            s.insert(i, ".");
+            backtracking(rt, s, i+1, level+1);
+            s.erase(i, 1);
+        }
+    }
+    
+    bool check(string s){       // check if the string is valid or not
+        int left = 0, right = 0;
+        while(right < s.size()){
+            left = right; right++;
+            while(right < s.size() && s[right++] !='.')
+
+            if(right - left == 2 && s[left] == '0') return false;
+            else if(right - left == 3){
+                if(s[left] == '0') return false;
+                if(s[left] > '2') return false;
+                if(s[left] == '2' && s[left+1] > '5') return false;
+                if(s[left] == '2' && s[left+1] == '5' && s[right-1] > '5') return false;
             }
-            else{
+        }
+        return true; 
+        
+        
+        
+   /* Approach 2 
+   bool isValid(string s){
+        if(s.size() > 1 && s[0] == '0')
+            return false;
+        int num = stoi(s);
+        return num >=0 && num <= 255;
+    }
+    void backtrack(vector<string> &ans, string san, string s, int count){
+        if(count > 4)
+            return;
+        if(count ==4 && s.size()==0){
+            san.pop_back();
+            ans.push_back(san);
+            return;
+        }
+        else if(count == 4 || s.size() == 0)
+            return;
+        for(int i = 1; i < 4 && i < s.size()+1; i++){
+            string sub = s.substr(0,i);
+            if(isValid(sub))
+                backtrack(ans, san+sub+'.', s.substr(i), count+1);
+            else
                 continue;
-            }
         }
     }
     vector<string> restoreIpAddresses(string s) {
         vector<string> ans;
-        string san="";
-        backtrack(ans, san,0,s);
+        string san = "";
+        backtrack(ans, san, s, 0);
         return ans;
+    */
     }
 };
