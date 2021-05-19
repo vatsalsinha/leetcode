9046b3c85aece6a58d9180fd2b221b8aa6a5a619
Vatsal <vatsalsinha001@gmail.com>
2021-05-19 16:32:51 +0530

    skyline problem updated

--- theSkylineProblem.cpp
+++ theSkylineProblem.cpp
@@ -44,5 +44,34 @@ public:
         }
         
         return contour;
+	/*
+	vector<pair<int, int>> edges;
+        int left, right, height;
+        for(int i = 0; i < buildings.size(); i++){
+            left = buildings[i][0];
+            right = buildings[i][1];
+            height = buildings[i][2];
+            edges.push_back(make_pair(left, -height));
+            edges.push_back(make_pair(right, height));
+        }
+        sort(edges.begin(), edges.end());
+        vector<vector<int>> result;
+        multiset<int> m;
+        m.insert(0);
+        int pre = 0, cur = 0;
+        for(int i = 0; i < edges.size(); i++){
+            pair<int, int> e = edges[i];
+            if(e.second < 0)
+                m.insert(-e.second);
+            else
+                m.erase(m.find(e.second));
+            cur = *m.rbegin();
+            if(cur != pre){
+                result.push_back({e.first, cur});
+                pre = cur;
+            }
+        }
+        return result;
+	*/
     }
 };
