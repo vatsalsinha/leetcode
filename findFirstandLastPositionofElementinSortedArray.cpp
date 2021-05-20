70218795801bafdec31677e1fd8b7559e511050e
Vatsal <vatsalsinha001@gmail.com>
2021-05-20 12:28:53 +0530

    find first and last position of element in sorted array with logN complexity

--- findFirstandLastPositionofElementinSortedArray.cpp
+++ findFirstandLastPositionofElementinSortedArray.cpp
@@ -1,28 +1,61 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int> ans(2, -1);
+//         int l = 0, r = nums.size()-1;
+//         int c = count(nums.begin(), nums.end(), target);
+//         c = c-1;
+//         int pos = -1;
+//         while (l <= r) {
+//         int m = l + (r - l) / 2;
+  
+//         // Check if x is present at mid
+//         if (nums[m] == target)
+//             pos = m;
+  
+//         // If x greater, ignore left half
+//         if (nums[m] < target)
+//             l = m + 1;
+  
+//         // If x is smaller, ignore right half
+//         else
+//             r = m - 1;
+//     }
+//         if(pos != -1){
+//             ans.clear();
+//             ans.push_back(pos);
+//             ans.push_back(pos+c);
+//             return ans;
+//         }
+//         return ans;
         vector<int> ans(2, -1);
-        int l = 0, r = nums.size()-1;
-        int c = count(nums.begin(), nums.end(), target);
-        c = c-1;
-        int pos = -1;
-        while (l <= r) {
-        int m = l + (r - l) / 2;
-        if (nums[m] == target)
-            pos = m;
-
-        if (nums[m] < target)
-            l = m + 1;
-
-        else
-            r = m - 1;
-    }
-        if(pos != -1){
-            ans.clear();
-            ans.push_back(pos);
-            ans.push_back(pos+c);
+        if(nums.size() == 0)
+            return ans;
+        int i = 0, j = nums.size()-1;
+        while(i < j){
+            int mid = i + (j-i) / 2;
+            if(nums[mid] < target)
+                i = mid + 1;
+            else 
+                j = mid;
+        }
+        if(nums[i] != target){
             return ans;
         }
+        else{
+            ans[0] = i;
+        }
+        j = nums.size() - 1;
+        while(i < j){
+            int mid = i+1 + (j-i) / 2;
+            if(nums[mid] > target){
+                j = mid - 1;
+            }
+            else{
+                i = mid;
+            }
+        }
+        ans[1] = j;
         return ans;
     }
 };
