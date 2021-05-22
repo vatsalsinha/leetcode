2014d54a420bcd385df7e3568d8440f921794f6a
Vatsal <vatsalsinha001@gmail.com>
2021-05-22 17:43:50 +0530

    n-ary tree level order traversal

---
+++ N-aryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if(root == NULL)
+            return {};
+        vector<vector<int>> ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            vector<int> res;
+            int n = q.size();
+            for(int i = 0; i < n; i++){
+                Node* tmp = q.front();
+                q.pop();
+                res.push_back(tmp->val);
+                for(auto N : tmp->children){
+                    q.push(N);
+                }
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
