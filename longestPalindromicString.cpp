0403cec4949216a5ba1ad190cddd24904c233ded
Vatsal <vatsalsinha001@gmail.com>
2021-05-29 16:45:30 +0530

    longest palindromic Substring updated

--- longestPalindromicString.cpp
+++ longestPalindromicString.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length()==1){
-            return s;
-        }
-        bool dp[s.length()][s.length()];
-        memset(dp, 0, sizeof(dp));
-        string result = "";
-        for(int g = 0; g < s.length(); g++){
-            for(int i = 0, j = g; j < s.length(); i++,j++){
-                if(g == 0){
-                    dp[i][j] = true;
+        int longestPal = 0, low = 0, high = 0;
+        for(int i = 0; i < s.length(); i++){
+            int left = i, right = i;
+            while(left >= 0 && right <= s.length() && s[left] == s[right]){
+                if(longestPal < right -left + 1){
+                    low = left;
+                    high = right;
+                    longestPal = right-left+1;
                 }
-                else if(g ==1){
+                left--; right++;
+            }
+            left = i;
+            right = i+1;
+            while(left >= 0 && right <= s.length() && s[left] == s[right]){
+                if(longestPal < right - left + 1){
+                    low = left;
+                    high = right;
+                    longestPal = right - left + 1;
+                }
+                left--; right++;
+            }
+        }
+        return s.substr(low, high-low+1);
+       /* bool dp[s.length()][s.length()];
+        memset(dp, 0, sizeof(dp));
+        string result;
+        for(int g = 0; g < s.length(); g++){
+            for(int i = 0, j = g; j < s.length(); i++, j++){
+                if(g == 0)
+                    dp[i][j] = true;
+                else if(g == 1){
                     if(s[i] == s[j]){
-                            dp[i][j] = true;
+                        dp[i][j] = true;
                     }
                     else{
                         dp[i][j] = false;
@@ -29,10 +48,10 @@ public:
                     }
                 }
                 if(dp[i][j])
-                    result = s.substr(i, j-i+1);
+                result = s.substr(i, j-i+1);
             }
         }
         return result;
+        */
     }
 };
-
