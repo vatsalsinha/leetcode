9f635682c2b601c81b5a282e77a73f8a9dcf48c5
Vatsal <vatsalsinha001@gmail.com>
2021-06-11 12:42:35 +0530

    excel sheet column number

---
+++ excelSheetColumnNumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res = 0;
+        for(int i = 0; i < columnTitle.size(); i++){
+            res = res * 26 + (columnTitle[i] - 'A' +1);
+        }
+        return res;
+    }
+};
