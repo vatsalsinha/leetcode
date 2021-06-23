85c6f376bdc1c67e6a68558e04015774d844cd9f
Vatsal <vatsalsinha001@gmail.com>
2021-06-23 17:45:03 +0530

    1396. Design Underground System

---
+++ designUndergroundSystem.cpp
@@ -0,0 +1,33 @@
+class UndergroundSystem {
+public:
+    unordered_map<int, pair<string,int>> checkInMap;
+    unordered_map<string, pair<int,int>> checkOutMap;
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        checkInMap[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        auto& it = checkInMap[id];
+        string route = it.first + "_" + stationName;
+        checkOutMap[route].first += t - it.second;
+        checkOutMap[route].second += 1;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        string route = startStation+"_"+endStation;
+        auto& it = checkOutMap[route];
+        return (double) it.first/it.second;
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
