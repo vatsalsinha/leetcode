bb5331498f580ec23ebc1402a8d1f8e9034d4353
Vatsal <vatsalsinha001@gmail.com>
2021-06-24 18:11:17 +0530

    419. Battleships in a Board

---
+++ battleShipsInABoard.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int i, int j){
+        if(i >= board.size() || j >= board[0].size() || i < 0 || j < 0 || board[i][j] == '.')
+            return;
+        board[i][j] = '.';
+        
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+        
+    }
+    int countBattleships(vector<vector<char>>& board) {
+        int count = 0;
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                if(board[i][j] == 'X'){
+                    count++;
+                    dfs(board, i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
