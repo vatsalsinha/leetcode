e66b709e91eb022e325fbbd29950252f8b0166dd
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-08-17 00:54:53 +0530

    dfs of a graph

---
+++ dfsOfAGraph.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution 
+{
+    public:
+	//Function to return a list containing the DFS traversal of the graph.
+	vector<int> ans;
+	void dfs(int V, vector<int>adj[], vector<bool>& visited){
+	    if(visited[V])
+	        return;
+	    visited[V] = true;
+	    ans.push_back(V);
+	    for(auto e : adj[V]){
+	        if(!visited[e])
+	            dfs(e, adj, visited);
+	    }
+	}
+	vector<int>dfsOfGraph(int V, vector<int> adj[])
+	{
+	    // Code here
+	    vector<bool> visited(V);
+	    dfs(0, adj, visited);
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    		adj[v].push_back(u);
+    	}
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int>ans=obj.dfsOfGraph(V, adj);
+        for(int i=0;i<ans.size();i++){
+        	cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
