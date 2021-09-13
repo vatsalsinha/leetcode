4e7659bd788719ff87d7a6e9bcf4c73e12d90657
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-09-13 14:53:43 +0530

    882. Reachable Nodes In Subdivided Graph(updated)

--- reachableNodesInSubdividedGraph.cpp
+++ reachableNodesInSubdividedGraph.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
-        vector<int> dist(n, INT_MAX);
-        unordered_map<int, unordered_map<int, int>> g;
+        vector<vector<pair<int,int>>> g(n);
         for(auto e : edges){
-            g[e[0]][e[1]] = g[e[1]][e[0]] = e[2] + 1;
+            g[e[0]].push_back({e[1], e[2]});
+            g[e[1]].push_back({e[0], e[2]});
         }
-       auto cmp = [](const pair<int, int>& a, const pair<int, int>& b){ return a.second > b.second; };
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-        pq.push({0,0});
+	//dijkstra starts
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0,0}); // node:weight
+        vector<int> dist(n, INT_MAX);
         dist[0] = 0;
         while(!pq.empty()){
-            auto u = pq.top().first;
+            int u = pq.top().first;
+            int w = pq.top().second;
             pq.pop();
-            if(dist[u] >= maxMoves)
-                break;
-            for(auto n : g[u]){
-                int v = n.first, w = n.second;
-                if(dist[u] + w < dist[v]){
-                    dist[v] = w + dist[u];
-                    pq.push({v, dist[v]});
+            for(auto i : g[u]){
+                if(dist[i.first] > w + i.second + 1){
+                    dist[i.first] = w + i.second + 1;
+                    pq.push({i.first, dist[i.first]});
                 }
             }
         }
-        int res = 0;
-        for(int i = 0; i < n; i++){
+	//dijsktra ends
+        int ans = 0;
+        for(int i = 0; i < n; i++){ // reachable nodes that are present in the initial graph
             if(dist[i] <= maxMoves)
-                res++;
+                ans++;
         }
-        for(auto e : edges){
-            int a = dist[e[0]] >= maxMoves ? 0 : min(maxMoves- dist[e[0]], e[2]);
-            int b = dist[e[1]] >= maxMoves ? 0 : min(maxMoves - dist[e[1]], e[2]);
-            res += min(a+b, e[2]);
+        for(auto e : edges){ // reachable nodes that are added later to the graph
+            int src = e[0], dst = e[1], bw = e[2];
+            int x = max(0, (maxMoves - dist[src]));
+            int y = max(0, (maxMoves - dist[dst]));
+            ans += min(bw, x+y);
         }
-        return res;
+        return ans;
     }
 };
