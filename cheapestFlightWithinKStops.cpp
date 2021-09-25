45157adf6f270b1019f77365d289de672a1efa54
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-09-25 23:00:23 +0530

    787. Cheapest Flights Within K Stops

--- cheapestFlightWithinKStops.cpp
+++ cheapestFlightWithinKStops.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, 1e8);
+        vector<int> dist(n, 1e8); // stores cost using at most k-1 edges
         if(k == 0){
             for(auto f : flights){
                 if(f[0] == src && f[1] == dst)
@@ -11,8 +11,8 @@ public:
             return -1;
         }
         dist[src] = 0;
-        for(int i = 0; i < k+1; i++){
-            vector<int> d(dist);
+        for(int i = 0; i < k+1; i++){ // k stops ie k+2 edges
+            vector<int> d(dist); //stores cost using at most k edges
             for(auto e : flights){
                 if(dist[e[0]] + e[2] < d[e[1]]){
                     d[e[1]] = dist[e[0]] + e[2];
