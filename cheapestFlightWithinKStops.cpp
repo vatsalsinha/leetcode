2d5fc17162da6c65be74e5de4e72eb538c8b515c
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-09-25 18:55:52 +0530

    Cheapest Flights Within K Stops (bellman ford algorithm)

---
+++ cheapestFlightWithinKStops.cpp
@@ -0,0 +1,25 @@
+//Bellman ford algorithm
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n, 1e8);
+        if(k == 0){
+            for(auto f : flights){
+                if(f[0] == src && f[1] == dst)
+                    return f[2];
+            }
+            return -1;
+        }
+        dist[src] = 0;
+        for(int i = 0; i < k+1; i++){
+            vector<int> d(dist);
+            for(auto e : flights){
+                if(dist[e[0]] + e[2] < d[e[1]]){
+                    d[e[1]] = dist[e[0]] + e[2];
+                }
+            }
+            dist = d;
+        }
+        return dist[dst] == 1e8 ? -1 : dist[dst];
+    }
+};
