67fe4cd035ed2789f4ee6ec840459f234c89314e
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-10-09 19:45:18 +0530

    78. Subsets using recursion and backtracking

--- subsets.cpp
+++ subsets.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
-    void backtrack(vector<vector<int>> &ans, vector<int> &res, vector<int> nums, int index){
-        ans.push_back(res);
-        for(int i = index; i < nums.size(); i++){
-            res.push_back(nums[i]);
-            backtrack(ans, res, nums, i+1);
-            res.pop_back();
+    // void backtrack(set<vector<int>> &ans, vector<int>& res, vector<int> nums, int index){
+    //     ans.insert(res);
+    //     for(int i = index; i < nums.size(); i++){
+    //         res.push_back(nums[i]);
+    //         backtrack(ans, res, nums, i+1);
+    //         res.pop_back();
+    //     }
+    // }
+    void findSubsets(vector<int>& nums, int n, int index, vector<int> tmp, vector<vector<int>>& ans, int sz){
+        if(index == n){
+            ans.push_back(tmp);
+            return;
         }
+        tmp.push_back(nums[index]);
+        findSubsets(nums, n, index+1, tmp, ans, sz+1);
+        tmp.pop_back();
+        findSubsets(nums, n, index+1, tmp, ans, sz);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        // sort(nums.begin(), nums.end());
+        // set<vector<int>> ans;
+        // vector<int> res;
+        // backtrack(ans, res, nums, 0);
+        // vector<vector<int>> a(ans.begin(), ans.end());
+        // return a;
+        
         vector<vector<int>> ans;
-        vector<int> res;
-        backtrack(ans, res, nums, 0);
+        vector<int> tmp;
+        int n = nums.size();
+        findSubsets(nums, n, 0, tmp, ans, 0);
         return ans;
     }
 };
