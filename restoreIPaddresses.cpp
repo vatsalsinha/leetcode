33244b1f2fb2de000c0c0fcdb3dac4402297b116
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2021-11-19 03:16:29 +0530

    93. Restore IP Addresses

--- restoreIPaddresses.cpp
+++ restoreIPaddresses.cpp
@@ -1,75 +1,73 @@
 class Solution {
 public:
-    vector<string> restoreIpAddresses(string s) {
-        vector<string> rt;
-        
-        backtracking(rt, s, 0, 1);
-        return rt;
-    }
-    
-    // level indicates how many '.'s inserted
-    void backtracking(vector<string> &rt, string& s, int index, int level){
+//recursion
+    void restore(string ip, string s, vector<string>& ans, int index, int level){
+        //cout<<ip<<"\n";
         if(level == 4){
-            if(s.size()-index <= 3 && s.size()-index > 0 && check(s))
-                rt.push_back(s);
+            if(index == s.length())
+                ans.push_back(ip.substr(0, ip.size()-1));
             return;
         }
-        
-        for(int i=index+1; i<=index+3 && i<s.size(); i++){
-            s.insert(i, ".");
-            backtracking(rt, s, i+1, level+1);
-            s.erase(i, 1);
-        }
-    }
-    
-    bool check(string s){       // check if the string is valid or not
-        int left = 0, right = 0;
-        while(right < s.size()){
-            left = right; right++;
-            while(right < s.size() && s[right++] !='.')
-
-            if(right - left == 2 && s[left] == '0') return false;
-            else if(right - left == 3){
-                if(s[left] == '0') return false;
-                if(s[left] > '2') return false;
-                if(s[left] == '2' && s[left+1] > '5') return false;
-                if(s[left] == '2' && s[left+1] == '5' && s[right-1] > '5') return false;
-            }
-        }
-        return true; 
-        
-        
-        
-   /* Approach 2 
-   bool isValid(string s){
-        if(s.size() > 1 && s[0] == '0')
-            return false;
-        int num = stoi(s);
-        return num >=0 && num <= 255;
-    }
-    void backtrack(vector<string> &ans, string san, string s, int count){
-        if(count > 4)
-            return;
-        if(count ==4 && s.size()==0){
-            san.pop_back();
-            ans.push_back(san);
-            return;
-        }
-        else if(count == 4 || s.size() == 0)
-            return;
-        for(int i = 1; i < 4 && i < s.size()+1; i++){
-            string sub = s.substr(0,i);
-            if(isValid(sub))
-                backtrack(ans, san+sub+'.', s.substr(i), count+1);
-            else
-                continue;
+        int num = 0;
+        for(int i = index; i < index+3 && i <= s.length(); i++){
+            //string tp = s.substr(index, i);
+            num = num*10 + (s[i]-'0');
+          //  if(stoi(s.substr(index, i)) <= 255)
+            if(num > 255)
+                return;
+            restore(ip + to_string(num) + ".",s,ans, i+1,level+1);
+            if(s[index] == '0') // since there can be no leading zeros
+                break;
         }
     }
     vector<string> restoreIpAddresses(string s) {
         vector<string> ans;
-        string san = "";
-        backtrack(ans, san, s, 0);
+        restore("", s, ans, 0, 0);
         return ans;
-    */
     }
 };
+
+/* Alternative solutions using backtracking
+class Solution {
+public:
+    bool check(string s){
+        int left = 0, right = 0;
+        while(right < s.length()){
+            left = right;
+            right++;
+            while(right < s.length() && s[right++] != '.')
+                if(right - left == 2 && s[left] == '0')
+                    return false;
+                else if(right - left == 3){
+                    if(s[left] == '0')
+                        return false;
+                    if(s[left] > '2')
+                        return false;
+                    if(s[left] == '2' && s[left+1] > '5')
+                        return false;
+                    if(s[left] == '2' && s[left+1] == '5' && s[right-1] > '5')
+                        return false;
+                }
+        }
+        return true;
+    }
+    void backtrack(vector<string> &ans, string &s, int index, int level){
+        if(level == 4){
+            if(s.length()-index <=3 && s.length()-index > 0 && check(s)){
+                ans.push_back(s);
+            }
+            return;
+        }
+        for(int i = index+1; i <= index + 3 && i < s.length(); i++){
+            s.insert(i, ".");
+            backtrack(ans, s, i+1, level+1);
+            s.erase(i,1);
+        }
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        backtrack(ans, s, 0, 1);
+        return ans;
+    }
+};
+*/
