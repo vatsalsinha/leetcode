396e3f70b9ec11e83a136178138d4e14cf975de5
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2022-06-08 20:07:31 +0530

    1332. Remove Palindromic Subsequences

---
+++ removePalindromicSubsequences.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int i = 0, j = s.size()-1;
+        while(i<j){
+            if(s[i++] != s[j--])
+                return 2;
+        }
+        return 1;
+    }
+};
