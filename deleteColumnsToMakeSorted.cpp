aa1c1fcbd74c3659eaa91c6f7008516e9f72ee79
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2023-01-05 00:09:48 +0530

    944. Delete Columns to Make Sorted

--- deleteColumnsToMakeSorted.cpp
+++ deleteColumnsToMakeSorted.cpp
@@ -3,8 +3,7 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int delete_count=0;
         int row = strs.size();
-        int col = strs[0].size();
-        
+        int col = strs[0].size();        
         for(int j=0; j<col; j++)
         {
             for(int i=0; i<row-1; i++)
