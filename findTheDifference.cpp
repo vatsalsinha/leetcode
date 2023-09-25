468ac621ed2d1a83b1b5c35218900fd168f63c90
Vatsal Sinha <57571075+vatsalsinha@users.noreply.github.com>
2023-09-25 12:12:06 +0530

    389. Find the Difference

--- findTheDifference.cpp
+++ findTheDifference.cpp
@@ -11,16 +11,23 @@ public:
         // }
         // return t[t.size()-1];
 
-        unordered_map<char, int> mp;
-        for(auto c : t)
-            mp[c]++;
-        for(auto c : s){
-           mp[c]--;
-        }
-        for(auto it : mp){
-            if(it.second == 1)
-                return it.first;
-        }
-        return 'a';
+        // unordered_map<char, int> mp;
+        // for(auto c : t)
+        //     mp[c]++;
+        // for(auto c : s){
+        //    mp[c]--;
+        // }
+        // for(auto it : mp){
+        //     if(it.second == 1)
+        //         return it.first;
+        // }
+        // return 'a';
+
+        char c = 0;
+        for(auto i : s)
+            c ^= i;
+        for(auto i : t)
+            c ^= i;
+        return c;
     }
 };
